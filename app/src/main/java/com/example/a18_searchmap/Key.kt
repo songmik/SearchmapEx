299b49d08282614a9f56e5bc91660b21a13a0d76
Songmi <ksongmi0727@gmail.com>
2022-01-29 23:07:57 +0900

    0129

---
+++ app/src/main/java/com/example/a18_searchmap/Key.kt
@@ -0,0 +1,7 @@
+package com.example.a18_searchmap
+
+object Key {
+    const val TMAP_API = "l7xx04d16910b78b40d7ae46d09061a6487c"
+
+
+}
---
+++ app/src/main/java/com/example/a18_searchmap/utillity/RetrofitUtil.kt
@@ -0,0 +1,35 @@
+package com.example.a18_searchmap.utillity
+
+import com.example.a18_searchmap.BuildConfig
+import com.example.a18_searchmap.Url
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+object RetrofitUtil {
+
+    val apiService: ApiService by lazy { getRetrofit().create(ApiService::class.java) }
+
+    private fun getRetrofit(): Retrofit {
+
+        return Retrofit.Builder()
+            .baseUrl(Url.TMAP_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(buildOkHttpClient())
+            .build()
+    }
+
+    private fun buildOkHttpClient(): OkHttpClient {
+        val interceptor = HttpLoggingInterceptor()
+        if (BuildConfig.DEBUG) {
+            interceptor.level = HttpLoggingInterceptor.Level.BODY
+        } else {
+            interceptor.level = HttpLoggingInterceptor.Level.NONE
+        }
+        return OkHttpClient.Builder()
+            .connectTimeout(5, TimeUnit.SECONDS)
+            .addInterceptor(interceptor)
+            .build()
+    }
+}
